b07809ffda71b71df67351e8583f6c9c3f4c0f2f
itsGhost, LLC <ceo@itsghostyt.com>
2017-12-15 12:10:23 -0500

    Redirect

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <title>Redirect</title>
 <script>
-document.write("Redirecting if redirect doesn't work please go to <a href='https://mynamesg.host/'></a>")
+document.write("Redirecting if redirect doesn't work please go to https://mynamesg.host/")
 </script>
 <meta http-equiv="refresh" content="3;url=https://mynamesg.host/" />
 <noscript>Your browser does not support JavaScript!</noscript>
