9cc85c5189236651657c9bd693f3512c067083ca
itsGhost, LLC <ceo@itsghostyt.com>
2017-12-15 12:03:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 <script>
 document.write("Redirecting if redirect doesn't work please go to <a href='https://mynamesg.host/'></a>")
 </script>
-<meta http-equiv="refresh" content="0;url=https://mynamesg.host/" />
+<meta http-equiv="refresh" content="3;url=https://mynamesg.host/" />
 <noscript>Your browser does not support JavaScript!</noscript>
