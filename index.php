a25716182c563f31f0a3c6c69826e1d73859a8fc
itsGhost, LLC <ceo@itsghostyt.com>
2017-12-15 12:08:29 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<title>Redirect</title>
+<script>
+document.write("Redirecting if redirect doesn't work please go to <a href='https://mynamesg.host/'></a>")
+</script>
+<meta http-equiv="refresh" content="3;url=https://mynamesg.host/" />
+<noscript>Your browser does not support JavaScript!</noscript>
